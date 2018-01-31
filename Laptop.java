251b938abd030f69ff2c0d0a374fef186384b189
Lemon <s.lemon.2016@mymail.jcsu.edu>
2018-01-31 13:15:26 -0500

    first commit

---
+++ Laptop.java
@@ -0,0 +1,14 @@
+
+public class Laptop {
+	String Laptop;
+	String OS;
+	int year;
+		public Laptop(String LapTop2,String OS7, int years) {
+				// TODO Auto-generated constructor stub
+			
+			Laptop=LapTop2;
+			OS=OS7;
+			year=years;	
+		}
+
+}
---
+++ LaptopTest.java
@@ -0,0 +1,18 @@
+
+public class LaptopTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	Laptop L=new Laptop("Toshiba ","Windows",2015);	
+		
+	Laptop M=new Laptop("Apple ","Mac ",2013);	
+		
+	System.out.println(M.Laptop+M.OS+M.year);	
+		
+		
+		
+
+	}
+
+}
