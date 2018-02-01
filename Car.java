8e40c8d208cb505e1dd85ff0eb20d140e483faac
Lemon <s.lemon.2016@mymail.jcsu.edu>
2018-02-01 13:06:47 -0500

    first commit

---
+++ Car.java
@@ -0,0 +1,16 @@
+
+public class Car {
+String carbrand;
+int price;
+int year;
+	public Car(String Cbrand,int priceZ,int years) {
+		// TODO Auto-generated constructor stub
+	carbrand=Cbrand;
+	price= priceZ;
+	year=years;
+		
+		
+		
+	}
+}
+	
---
+++ Cartest.java
@@ -0,0 +1,15 @@
+
+public class Cartest {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	Car C=new Car("Saturn ", 600 , 2015);			
+		
+	Car r=new Car("Lexus ", 8000, 2017);		
+		
+	System.out.println(r.carbrand+r.price +r.year);
+	
+	}
+
+}
+
